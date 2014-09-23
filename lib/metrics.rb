6d9ce9a1faf7c43a70080d5ef749d9b699ea0aba
mijikim <miji.kim926@gmail.com>
2014-09-23 15:30:40 -0600

    fixed data issue with finding avgs

--- lib/metrics.rb
+++ lib/metrics.rb
@@ -5,76 +5,80 @@ class Metrics
   attr_reader :filepath
 
   def initialize(filepath)
-  @filepath = filepath
+    @filepath = filepath
   end
 
   def parse
-    CSV.read(@filepath, { :col_sep => "\t" })
+    CSV.read(@filepath, {:col_sep => "\t"})
   end
 
   def create_hash_with_keys(array_of_arrays)
-    hash = {}
+    nested_hash = Hash.new do |hash, key|
+      hash[key] = {}
+    end
+
     array_of_arrays.each { |row|
-      hash.compare_by_identity
-
-      hash[:time] = row[0]
-      hash[:name] = row[1]
-      hash[:pH] = row[2]
-
-      # hash[row[1]] = row
+      nested_hash.compare_by_identity
+      main_key = row[1]
+      nested_hash[main_key]["time"] = row[0]
+      nested_hash[main_key]["name"] = row[1]
+      nested_hash[main_key]["pH"] = row[2]
+      nested_hash[main_key]["nutrient_solution_level"] = row[3]
+      nested_hash[main_key]["temperature"] = row[4]
+      nested_hash[main_key]["water_level"] = row[5]
     }
 
-    ap hash
-    # new_hash = (hash.group_by {|row| row[0]}).each { |key, values| values.map! {|key, value| values.delete(key) & value.delete(value[1]); value }}
-    #
-    # hash = {}
-    # hash_with_keys = new_hash.each {|key, values| values.map! {|item|
-    #   hash.compare_by_identity
-    # hash[:time] = item[0]
-    # hash[:pH] = item[1]
-    # hash[:nutrient_solution_level] = item[2]
-    # hash[:temperature] = item[3]
-    # hash[:water_level] = item[4] ; hash}}
-
+    nested_hash.group_by { |row| row[0] }.each { |key, values| values.map! { |key, value| values.delete(key) & value.delete(value[1]); value } }
   end
 
-  # def return_hash_with_avgs(hash)
-  #   pH = 0.00
-  #   nutrient_solution_level = 0.00
-  #   temp = 0.00
-  #   h2o_level = 0.00
-  #   count = 0.00
-  #   new_hash = {}
-  #   hash["container1"].map! { |values|
-  #   pH += values[:pH].to_f
-  #   nutrient_solution_level += values[:nutrient_solution_level].to_f
-  #   temp += values[:temperature].to_f
-  #   h2o_level += values[:water_level].to_f
-  #   count += 1 }
-  #   Hash.new["container1"] =
-  #   (new_hash[:temp]= pH/count
-  #   new_hash[:nutrient_solution_level] = nutrient_solution_level/count
-  #   new_hash[:temperature] = temp/count
-  #   new_hash[:water_level] = h2o_level/count)
-  # new_hash
-  # end
+  def return_hash_with_avgs(hash)
+    nested_hash = Hash.new do |h, key|
+      h[key] = {}
+    end
 
+    pH = 0
+    nutrient_solution_level = 0
+    temp = 0
+    h2o_level = 0
+    count = 0
+    hash["container1"].each { |array|
+    count += 1
+    nested_hash["container1"]["pH"] = ((pH += array["pH"].to_f)/count).round(2)
+    nested_hash["container1"]["nutrient_solution_level"] = ((nutrient_solution_level += array["nutrient_solution_level"].to_f)/count).round(2)
+    nested_hash["container1"]["temperature"] = ((temp += array["temperature"].to_f)/count).round(2)
+    nested_hash["container1"]["water_level"] = ((h2o_level += array["water_level"].to_f)/count).round(2)
+    }
 
+    pH = 0
+    nutrient_solution_level = 0
+    temp = 0
+    h2o_level = 0
+    count = 0
+    hash["container2"].each { |array|
+      count += 1
+      nested_hash["container2"]["pH"] = ((pH += array["pH"].to_f)/count).round(2)
+      nested_hash["container2"]["nutrient_solution_level"] = ((nutrient_solution_level += array["nutrient_solution_level"].to_f)/count).round(2)
+      nested_hash["container2"]["temperature"] = ((temp += array["temperature"].to_f)/count).round(2)
+      nested_hash["container2"]["water_level"] = ((h2o_level += array["water_level"].to_f)/count).round(2)
+    }
+
+    pH = 0
+    nutrient_solution_level = 0
+    temp = 0
+    h2o_level = 0
+    count = 0
+    hash["container3"].each { |array|
+      count += 1
+      nested_hash["container3"]["pH"] = ((pH += array["pH"].to_f)/count).round(2)
+      nested_hash["container3"]["nutrient_solution_level"] = ((nutrient_solution_level += array["nutrient_solution_level"].to_f)/count).round(2)
+      nested_hash["container3"]["temperature"] = ((temp += array["temperature"].to_f)/count).round(2)
+      nested_hash["container3"]["water_level"] = ((h2o_level += array["water_level"].to_f)/count).round(2)
+    }
+    ap nested_hash
+  end
 
 
 end
 
 metrics = Metrics.new('/Users/MiJi/gSchoolWork/assessments/g3-assessment-week-16/data/metrics.tsv')
-# metrics.return_hash_with_avgs(metrics.create_hash_with_keys(metrics.parse))
-metrics.create_hash_with_keys(metrics.parse)
-
-  # def parse
-  #   open(@filepath) do |f|
-  #     headers = f.gets.strip.split("\t")
-  #     f.each do |line|
-  #       fields = Hash[headers.zip(line.split("\t"))]
-  #       yield fields
-  #     end
-  #   end
-  # end
-
+metrics.return_hash_with_avgs(metrics.create_hash_with_keys(metrics.parse))
